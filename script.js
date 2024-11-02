a8031ac0a5fde1e7c7749fcfd15e98d8ad0f2b3b
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2024-11-02 11:34:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ class Person {
 	  this.name = name;
 	  this.age = age;
 	}
+	greet() {
+	  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	};
 }
 
-greet() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
 
 class  Employee extends Person {
 	constructor(name, age, jobTitle) {
 		super(name, age);
 		this.jobTitle = jobTitle;
 	}
-
 	jobGreet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
